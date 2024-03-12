d89cacf1de4dfbe2ac0ac861dc9ec18fc773e293
aryankhatana01 <aryankhatana01@gmail.com>
2024-03-12 14:30:57 +0530

    added reqs

--- src/hyperparams.py
+++ src/hyperparams.py
@@ -3,7 +3,7 @@ import torch
 class HyperParams:
     IMAGE_H, IMAGE_W = 40, 40
     N_ITERS = 100
-    LR = 0.001
+    LR = 0.01
     BATCH_SIZE = 64
     N_EPOCHS = 10
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
